feaa9cba947e438da46aa66f518e392191714363
IPEASLEE <100698983+IPEASLEE@users.noreply.github.com>
2022-03-02 09:32:36 -0500

    initial commit

---
+++ projects/rollaball/FPS test/Assets/_scripts/playercontroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontroller : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public int score;
+    public Text scoreText;
+    public Text winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hMove = Input.GetAxis("Horizontal") * speed;
+        float vMove = Input.GetAxis("Vertical") * speed;
+        rb.AddForce(new Vector3(hMove, 0.0f, vMove));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("collectible"))
+        {
+            score++;
+            scoreText.text = "Score: " + score;
+            Destroy(other.gameObject);
+
+            if (score == 8)
+            {winText.text = "you win :D";
+
+            }
+        }
+    }
+}
---
+++ projects/rollaball/FPS test/Assets/menu_button_controls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu_button_controls : MonoBehaviour
+{
+    public void LoadOnClick()
+    {
+        SceneManager.LoadScene("level one");
+    }
+    public void QuitOnClick()
+    {
+        Application.Quit();
+    }
+}
